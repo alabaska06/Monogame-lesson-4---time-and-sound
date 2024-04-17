7bb8fca8ed2c965f5616c7b10c6b49ec087ce5fd
Alanna Smith <smitalan240@gmail.com>
2024-04-17 14:32:28 -0400

    idk how to make the explosion stop when the sound is done

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using static System.Formats.Asn1.AsnWriter;
@@ -9,6 +10,13 @@ namespace Monogame_lesson_4___time_and_sound
     {
 
         Texture2D bombTexture;
+        Texture2D boomTexture;
+
+        SoundEffect explode;
+
+        bool exploTime;
+
+        MouseState mouseState;
 
         float seconds;
 
@@ -36,6 +44,7 @@ namespace Monogame_lesson_4___time_and_sound
             _graphics.ApplyChanges();
 
             seconds = 0;
+            exploTime = false;
 
             base.Initialize();
         }
@@ -45,6 +54,8 @@ namespace Monogame_lesson_4___time_and_sound
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             bombTexture = Content.Load<Texture2D>("bomb");
             timefont = Content.Load<SpriteFont>("Time");
+            explode = Content.Load<SoundEffect>("explosion");
+            boomTexture = Content.Load<Texture2D>("boom");
 
 
             // TODO: use this.Content to load your game content here
@@ -52,13 +63,22 @@ namespace Monogame_lesson_4___time_and_sound
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed)
+                seconds = 0f;
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (seconds > 10) 
+            if (seconds >= 15)
+            {
+                explode.Play();
                 seconds = 0f;
-
+                exploTime = true;
+               
+            }
+            
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -70,7 +90,11 @@ namespace Monogame_lesson_4___time_and_sound
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(bombTexture, new Rectangle(50, 50, 700, 400), Color.White);
-            _spriteBatch.DrawString(timefont, (10 - seconds).ToString("00.0"), new Vector2(270, 200), Color.Black);
+            _spriteBatch.DrawString(timefont, (15 - seconds).ToString("00.0"), new Vector2(270, 200), Color.Black);
+            if (exploTime == true)
+            {
+                _spriteBatch.Draw(boomTexture, new Rectangle(100, 100, 400, 400), Color.White);
+            }
             _spriteBatch.End();
 
             // TODO: Add your drawing code here
